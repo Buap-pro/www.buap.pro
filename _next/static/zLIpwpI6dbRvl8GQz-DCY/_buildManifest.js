51e6a16df45091de2cdb323bdb1c8030e9c7dec1
Fer2G <123048581+Fer2G@users.noreply.github.com>
2026-01-09 02:40:00 -0600

    Actualización a commit (64de77705acffde03dc86c7817c97dce5bf13c25) del repositorio principal

--- _next/static/zLIpwpI6dbRvl8GQz-DCY/_buildManifest.js
+++ _next/static/zLIpwpI6dbRvl8GQz-DCY/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/www.buap.pro//_next/:path+",
-        "destination": "/www.buap.pro/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
